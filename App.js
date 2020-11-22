e091c06197ae5874b85a6f81b90ff1f3098d2c68
awantech-io <hello.wanlab@gmail.com>
2020-11-23 00:14:07 +0800

    list component

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Eggs'},
+    {id: uuidv4(), text: 'Bread'},
+    {id: uuidv4(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title='Vanilla Shop List'/>
+      <FlatList 
+       data={items}
+      renderItem={({item}) => <ListItem item={item} />}
+      />
     </View>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 
 const Header = ({title}) => {
+
   return (
     <View style={styles.header}>
       <Text style={styles.text}>{title}</Text>
