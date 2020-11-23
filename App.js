b6ef45419871ca342ae5b7e6ba4726363cd50fbc
awantech-io <hello.wanlab@gmail.com>
2020-11-23 13:33:07 +0800

    add list process

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import AddItem from './components/AddItem';
@@ -19,10 +19,22 @@ const App = () => {
     });
   }
 
+  const addItem = (text) => {
+    if(!text){
+      //Alert.alert('Error','Please enter an item', [{text:'ok'}]);
+      Alert.alert('No item  😕', 'Please enter an item.', [{text: 'Ok'}]);
+    } else {
+      setItems(prevItems => {
+        return [{id:uuidv4(), text}, ...prevItems];      
+      })
+
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header title='Shop List'/>
-      <AddItem />
+      <AddItem addItem={addItem} />
       <FlatList 
        data={items}
       renderItem={({item}) => (
--- components/AddItem.js
+++ components/AddItem.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const AddItem = ({title}) => {
+const AddItem = ({title, addItem}) => {
+  const [text, setText] = useState('');
+
+  const onChange = (textValue) => setText(textValue);
+  
   return (
     <View>
-      <TextInput placeholder="Add Item..." style={StyleSheet.input} />
-      <TouchableOpacity style={styles.btn}>
+      <TextInput placeholder="Add Item..." style={StyleSheet.input} 
+      onChangeText={onChange} />
+      <TouchableOpacity style={styles.btn} 
+      onPress={() =>addItem(text)}
+      >
       <Text style={styles.btnText}>
         <Icon name="plus" size={20} />
         Add Item</Text>
